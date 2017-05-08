bfe7f90d353d6cb74d5333af0489847f4940d26d
weizhengwei <iamchingwei@gmail.com>
2017-05-08 18:14:13 +0800

    [] to get()

--- healthroom.py
+++ healthroom.py
@@ -35,11 +35,11 @@ def uploadBloodPresure():
 		return 'uploadHeighWeight'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			bloodpresuredata = tb_bloodpresure(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], item['SBP'], item['DBP'], item['MBP'], item['pulse'], item['conclusion'])
+			bloodpresuredata = tb_bloodpresure(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), item.get('SBP'), item.get('DBP'), item.get('MBP'), item.get('pulse'), item.get('conclusion'))
 			db.session.add(bloodpresuredata)
 			db.session.commit()
 		return 'upload data ok'
@@ -50,11 +50,11 @@ def uploadBloodSugar():
 		return 'uploadHeighWeight'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			bloodsugardata = tb_bloodsugar(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], item['fastingBloodGlucose'],  item['conclusion'])
+			bloodsugardata = tb_bloodsugar(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), item.get('fastingBloodGlucose'),  item.get('conclusion'))
 			db.session.add(bloodsugardata)
 			db.session.commit()
 		return 'upload data ok'
@@ -65,26 +65,26 @@ def uploadBodyComposion():
 		return 'uploadBodyComposion'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			bwhdata = tb_bwh(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], 
-				item['abdominalBodyFatmass'], item['abdominalBodyFatmassAdjust'],item['trunkSoftleanmassFlag'],
-				item['visceralFatArea'], item['visceralFatmass'],item['weightAdjust'], item['weightHighLimit'],
-				item['weightlowlimit'], item['whr'],item['abdominalBodyFatmassHighLimit'], item['abdominalBodyFatmassLowLimit'],
-				item['adbominalSoftleanmass'], item['basicMetabolicrate'],item['bmi'], item['bmiHighLimit'],
-				item['bmiLowLimit'], item['bodyAge'],item['bodyFatRate'], item['bodyFatHeighLimit'],
-				item['bodyFatLowLimit'], item['bodyType'],item['extracellularFluid'], item['impedance'],
-				item['intracellularFluid'], item['leanBodymass'],item['leanBodymassHighLimit'], item['leanBodymassLowLimit'],
-				item['leftArmBodyFatmass'], item['leftArmSoftleanmass'],item['leftArmsoftleanmassFlag'], item['leftLegBodyFatmass'],
-				item['leftLegSoftleanmass'], item['leftLegSoftleanmassFlag'],item['massOfBodyFat'], item['mineral'],
-				item['mineralHighLimit'], item['mineralLowLimit'],item['obesexaxis'], item['protein'],
-				item['proteinHighLimit'], item['proteinLowLimit'],item['rightArmbodyFatmass'], item['rightArmSoftleanmassFlag'],
-				item['rightLegBodyFatmass'], item['rightLegSoftleanmassFlag'],item['rigtArmSoftleanmass'], item['rigtLegSoftleanmass'],
-				item['softleanmass'], item['softleanmassAdjust'],item['softleanmassHighLimit'], item['softleanmassLowLimit'],
-				item['standardWeight'], item['subcutaneousFatmass'],item['totalBodyWater'], item['totalBodyWaterHighLimit'],				
-				item['totalBodyWaterLowLimit'], item['totalEnergyConsumption'],item['conclusion'])
+			bwhdata = tb_bwh(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), 
+				item.get('abdominalBodyFatmass'), item.get('abdominalBodyFatmassAdjust'),item.get('trunkSoftleanmassFlag'),
+				item.get('visceralFatArea'), item.get('visceralFatmass'),item.get('weightAdjust'), item.get('weightHighLimit'),
+				item.get('weightlowlimit'), item.get('whr'),item.get('abdominalBodyFatmassHighLimit'), item.get('abdominalBodyFatmassLowLimit'),
+				item.get('adbominalSoftleanmass'), item.get('basicMetabolicrate'),item.get('bmi'), item.get('bmiHighLimit'),
+				item.get('bmiLowLimit'), item.get('bodyAge'),item.get('bodyFatRate'), item.get('bodyFatHeighLimit'),
+				item.get('bodyFatLowLimit'), item.get('bodyType'),item.get('extracellularFluid'), item.get('impedance'),
+				item.get('intracellularFluid'), item.get('leanBodymass'),item.get('leanBodymassHighLimit'), item.get('leanBodymassLowLimit'),
+				item.get('leftArmBodyFatmass'), item.get('leftArmSoftleanmass'),item.get('leftArmsoftleanmassFlag'), item.get('leftLegBodyFatmass'),
+				item.get('leftLegSoftleanmass'), item.get('leftLegSoftleanmassFlag'),item.get('massOfBodyFat'), item.get('mineral'),
+				item.get('mineralHighLimit'), item.get('mineralLowLimit'),item.get('obesexaxis'), item.get('protein'),
+				item.get('proteinHighLimit'), item.get('proteinLowLimit'),item.get('rightArmbodyFatmass'), item.get('rightArmSoftleanmassFlag'),
+				item.get('rightLegBodyFatmass'), item.get('rightLegSoftleanmassFlag'),item.get('rigtArmSoftleanmass'), item.get('rigtLegSoftleanmass'),
+				item.get('softleanmass'), item.get('softleanmassAdjust'),item.get('softleanmassHighLimit'), item.get('softleanmassLowLimit'),
+				item.get('standardWeight'), item.get('subcutaneousFatmass'),item.get('totalBodyWater'), item.get('totalBodyWaterHighLimit'),				
+				item.get('totalBodyWaterLowLimit'), item.get('totalEnergyConsumption'),item.get('conclusion'))
 			db.session.add(bwhdata)
 			db.session.commit()
 		return 'upload data ok'
@@ -95,13 +95,13 @@ def uploadBoneDensity():
 		return 'uploadBoneDensity'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			bonedensitydata = tb_bonedensity(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], item['acousticveLocity'], 
-				item['tValue'], item['zValue'], item['thScale'], item['toScale'], item['zYear'], item['riskLeavel'], 
-				item['oi'], item['youngAdult'], item['ageMatched'], item['bua'], item['opr'], item['conclusion'])
+			bonedensitydata = tb_bonedensity(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), item.get('acousticveLocity'), 
+				item.get('tValue'), item.get('zValue'), item.get('thScale'), item.get('toScale'), item.get('zYear'), item.get('riskLeavel'), 
+				item.get('oi'), item.get('youngAdult'), item.get('ageMatched'), item.get('bua'), item.get('opr'), item.get('conclusion'))
 			db.session.add(bonedensitydata)
 			db.session.commit()
 		return 'upload data ok'
@@ -112,12 +112,12 @@ def uploadBWH():
 		return 'uploadBWH'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			bwhdata = tb_bwh(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], 
-				item['hip'], item['bust'],item['waist'], item['conclusion'])
+			bwhdata = tb_bwh(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), 
+				item.get('hip'), item.get('bust'),item.get('waist'), item.get('conclusion'))
 			db.session.add(bwhdata)
 			db.session.commit()
 		return 'upload data ok'
@@ -128,13 +128,13 @@ def uploadEcg():
 		return 'uploadEcg'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			ecgdata = tb_ecg(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], item['HR'], item['PR'],
-				item['P_Duration'], item['T_Duration'], item['QT_Duration'], item['QTc_Duration'], item['P_Axis'], 
-				item['R_V5'], item['S_V1'], item['conclusion'])
+			ecgdata = tb_ecg(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), item.get('HR'), item.get('PR'),
+				item.get('P_Duration'), item.get('T_Duration'), item.get('QT_Duration'), item.get('QTc_Duration'), item.get('P_Axis'), 
+				item.get('R_V5'), item.get('S_V1'), item.get('conclusion'))
 			db.session.add(ecgdata)
 			db.session.commit()
 		return 'upload data ok'
@@ -145,12 +145,12 @@ def uploadElectronicVision():
 		return 'uploadElectronicVision'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			ecgdata = tb_ecg(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], item['checkType'],
-				item['leftEye'], item['rightEye'], item['conclusion'])
+			ecgdata = tb_ecg(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), item.get('checkType'),
+				item.get('leftEye'), item.get('rightEye'), item.get('conclusion'))
 			db.session.add(ecgdata)
 			db.session.commit()
 		return 'upload data ok'
@@ -161,11 +161,11 @@ def uploadHeighWeight():
 		return 'uploadHeighWeight'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			heighweightdata = tb_heighweightdata(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], 'as', 'asd', item['heigh'], item['weight'], item['bmi'], item['conclusion'])
+			heighweightdata = tb_heighweightdata(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), 'as', 'asd', item.get('heigh'), item.get('weight'), item.get('bmi'), item.get('conclusion'))
 		#heighweightdata = tb_heighweightdata()
 			db.session.add(heighweightdata)
 			db.session.commit()
@@ -177,19 +177,19 @@ def uploadLung():
 		return 'uploadlung'
 	elif request.method == 'POST':
 		data = request.get_json()
-		realdata = data['data']
+		realdata = data.get('data')
 		for item in realdata:
-			lungdata = tb_lung(data['familyCode'], data['familyName'], data['orgCode'], data['orgName'], 
-				data['dataSource'], data['machineID'], item['examDate'], item['residentEMPI'], item['residentEMPI'], 
-				item['residentName'], item['auditDoctorEMPI'], item['auditDoctorName'], 
-				item['FVC'], item['FEV1'],	item['FEV2'], 
-				item['FEV1Percent'], item['FEV2Percent'], item['FEV3Percent'], 
-				item['MMF'], item['MVV1'], item['BSA1'], 
-				item['M_B1'], item['PEF'], item['V75'], 
-				item['V50'], item['V25'], item['V50_V25'], item['V25_H'], 
-				item['MVV'], item['MVV_BSA'], item['VC'], item['TV'], 
-				item['IRV'], item['ERV'], item['IC'], item['MV'], item['RR'], 
-				item['Result'],	item['conclusion'])
+			lungdata = tb_lung(data.get('familyCode'), data.get('familyName'), data.get('orgCode'), data.get('orgName'), 
+				data.get('dataSource'), data.get('machineID'), item.get('examDate'), item.get('residentEMPI'), item.get('residentEMPI'), 
+				item.get('residentName'), item.get('auditDoctorEMPI'), item.get('auditDoctorName'), 
+				item.get('FVC'), item.get('FEV1'),	item.get('FEV2'), 
+				item.get('FEV1Percent'), item.get('FEV2Percent'), item.get('FEV3Percent'), 
+				item.get('MMF'), item.get('MVV1'), item.get('BSA1'), 
+				item.get('M_B1'), item.get('PEF'), item.get('V75'), 
+				item.get('V50'), item.get('V25'), item.get('V50_V25'), item.get('V25_H'), 
+				item.get('MVV'), item.get('MVV_BSA'), item.get('VC'), item.get('TV'), 
+				item.get('IRV'), item.get('ERV'), item.get('IC'), item.get('MV'), item.get('RR'), 
+				item.get('Result'),	item.get('conclusion'))
 			item.get('')
 			db.session.add(lungdata)
 			db.session.commit()
